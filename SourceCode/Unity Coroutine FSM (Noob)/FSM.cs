20e45997d655e6458b3131a0b773cdf2aeb027b4
Gengaozo <tmdrjf02@gmail.com>
2018-04-15 23:59:02 +0900

    FSM ADDED ATTACK

--- SourceCode/Unity Coroutine FSM (Noob)/FSM.cs
+++ SourceCode/Unity Coroutine FSM (Noob)/FSM.cs
@@ -8,6 +8,11 @@ public class FSM : MonoBehaviour {
 	Rigidbody2D rd2d;
 	int horizontal;
 	float walkStartTime;
+	float movementSpeed;
+
+	bool isFixed;
+	bool isMovable;
+	bool isAttacking;
 
 	// Use this for initialization
 	void Start () {
@@ -15,91 +20,154 @@ public class FSM : MonoBehaviour {
 		rd2d = GetComponent<Rigidbody2D>();
 
 		walkStartTime = 0;
+		movementSpeed = 1.5f;
+
+		isFixed = false;
+		isMovable = true;
+		isAttacking = false;
 
 		StartCoroutine("idle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		isFixed = false;
 		horizontal = (int)Input.GetAxisRaw("Horizontal");
+
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			isFixed = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{	
+			if(!isAttacking)
+				StartCoroutine("attack");
+		}
 	}
 
 	IEnumerator idle()
 	{
-		bool flag = true;
-		while(flag)
+		ani.SetTrigger("idle");
+		while(true)
 		{
-			if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+			//Debug.Log("idle");
+			if(isAttacking)
 			{
-				ani.ResetTrigger("idle");
-				ani.SetTrigger("walk");
-
-				flag = false;
+				break;
 			}
+
+			if (horizontal != 0)
+			{
+				StartCoroutine("walk");
+				break;
+			}
+
 			yield return new WaitForEndOfFrame();
 		}
-		StartCoroutine("walk");
+		ani.ResetTrigger("idle");
 	}
 
 	IEnumerator walk()
 	{
-		bool flag = true;
+		ani.SetTrigger("walk");
 		walkStartTime = Time.time;
-		while(flag)
+		while(true)
 		{
-			Debug.Log("walking");
-			if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+
+			if (isAttacking)
 			{
-				ani.ResetTrigger("walk");
-				ani.SetTrigger("idle");
-			
-				flag = false;
+				break;
 			}
 
-			if(Time.time - walkStartTime > 2)
+			if (horizontal == 0)
 			{
-				ani.ResetTrigger("walk");
-				ani.SetTrigger("run");
+				StartCoroutine("idle");
 
-				flag = false;
+				break;
 			}
-			move();
+
+			if(Time.time - walkStartTime > 2 || isFixed)
+			{
+				StartCoroutine("run");
+
+				break;
+			}
+			move(movementSpeed);
 
 			yield return new WaitForEndOfFrame();
 		}
-		StartCoroutine("idle");
+		ani.ResetTrigger("walk");
 	}
 
 	IEnumerator run()
 	{
-		bool flag = true;
-		while(flag)
+		ani.SetTrigger("run");
+		movementSpeed = 3f;
+		while(true)
 		{
-			if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+
+			if (isAttacking)
 			{
-				ani.ResetTrigger("run");
-				ani.SetTrigger("idle");
-				flag = false;
+				break;
 			}
-			move();
+			if (horizontal == 0 && !isFixed)
+			{
+				movementSpeed = 1f;
+				StartCoroutine("idle");
+				break;
+			}
+			move(movementSpeed);
 
 			yield return new WaitForEndOfFrame();
 		}
+		ani.ResetTrigger("run");
 	}
 
-	void move()
+	IEnumerator attack()
 	{
+		Debug.Log("attack!");
+
+		isAttacking = true;
+		ani.SetTrigger("attack");
+
+		yield return new WaitForSeconds(1);
+
+		while(ani.GetCurrentAnimatorStateInfo(0).IsName("skill_1"))
+		{
+			yield return new WaitForEndOfFrame();
+		}
+
+		isAttacking = false;
+		StartCoroutine("idle");
+
+		ani.ResetTrigger("attack");
+		yield return new WaitForEndOfFrame();
+	}
+
+	void move(float speed)
+	{
+		//Debug.Log(speed);
+
 		Vector2 newPos = transform.position;
 		Vector3 localScale = transform.localScale;
 		if(horizontal > 0)
 		{
 			localScale.x = Mathf.Abs(localScale.x);
-			newPos += Vector2.right * Time.deltaTime;
+			newPos += Vector2.right * Time.deltaTime * speed;
 		}
 		else if(horizontal < 0)
 		{
 			localScale.x = -Mathf.Abs(localScale.x);
-			newPos += Vector2.left * Time.deltaTime;
+			newPos += Vector2.left * Time.deltaTime * speed;
+		}
+		else if(isFixed)
+		{
+			Debug.Log("Fixed");
+			if(localScale.x > 0)
+				newPos += Vector2.right * Time.deltaTime * speed;
+			else if(localScale.x < 0)
+				newPos += Vector2.left * Time.deltaTime * speed;
 		}
 
 		transform.localScale = localScale;
